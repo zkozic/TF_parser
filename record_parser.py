ea888a213255483ef85175d7e7070e73ed955d83
Owen Dando <owen.dando@ed.ac.uk>
2019-04-08 16:51:32 +0100

    Fix case when gene symbol is not present.
    Also include full name of transcript factor rather than just family.

--- record_parser.py
+++ record_parser.py
@@ -8,6 +8,13 @@ warnings.filterwarnings("ignore")
 #gb_file = sys.argv[1]
 #gb_record = SeqIO.read(open(gb_file,"r"), "genbank")
 
+def get_gene_id(desc_dict):
+    if 'sym' in desc_dict:
+        return desc_dict['sym']
+
+    return desc_dict['acc']
+
+
 gene_list = []
 prom_starts_list = []
 prom_ends_list = []
@@ -21,16 +28,19 @@ comp_list = []
 
 for record in SeqIO.parse(open(gb_file,"r"), "genbank"):
     desc = record.description.split('|')
-    gene = [i for i in desc if 'sym' in i][0].split('=')[1]
-    chr = [i for i in desc if 'chr' in i][0].split('=')[1]
-    strand = re.split("\(|\)", [i for i in desc if 'str' in i][0].split('=')[1])[-2]
-    prom_start = [i for i in desc if 'start' in i][0].split('=')[1]
-    prom_end = [i for i in desc if 'end' in i][0].split('=')[1]
+    desc_dict = {entry.split('=')[0].strip():entry.split('=')[1].strip() for entry in desc}
+
+    gene = get_gene_id(desc_dict)
+    chromosome = desc_dict['chr']
+    strand = re.split("\(|\)", desc_dict['str'])[-2]
+    prom_start = desc_dict['start']
+    prom_end = desc_dict['end']
+
     for feature in record.features:
         feature = str(feature)
         fsplit = feature.split('\n')
         loc = re.split('\[|\]', [i for i in fsplit if 'location' in i][0])[1]
-        tf = re.split("'|/", [i for i in fsplit if 'Value' in i][0])[1]
+        tf = re.split("'|,", [i for i in fsplit if 'Value' in i][0])[2]
         bs_start = loc.split(':')[0]
         bs_end = loc.split(':')[1]
         score = re.split(" |'", [i for i in fsplit if 'Value' in i][0])[-2]
@@ -38,7 +48,7 @@ for record in SeqIO.parse(open(gb_file,"r"), "genbank"):
         if '-' in feature:
             comp = 'Complement'
         gene_list += [gene]
-        chr_list += [chr]
+        chr_list += [chromosome]
         strand_list += [strand]
         prom_starts_list += [prom_start]
         prom_ends_list += [prom_end]
